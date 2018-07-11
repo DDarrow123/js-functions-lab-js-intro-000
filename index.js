502132404fb224146536f7ee176eec6ac250c4e9
Devon Darrow <ddarrowphl@gmail.com>
2018-07-11 03:09:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// write your code below!
+function happyHolidays("Happy holidays!"){
+
+  return "Happy holidays!";
+
+}
+
+
+
+
+function happyHolidaysTo(Mom){
+
+  return `Happy holidays, ${name}!`
+
+}
+
+
+
+function happyHolidayTo(Labor Day, Mom){
+
+  return `Happy ${holiday}, ${name}!`
+}
+
+
+
+function holidayCountdown(Labor Day, 55 ){
+
+  return `It's ${days} days until ${holiday}!`
+}
